c90b9118261e3b610e0f3404555dabf8ff98ba1c
InnaMcGrane <innamcgrane@gmail.com>
2024-11-09 08:22:43 +0000

    create function createElement

--- script.js
+++ script.js
@@ -17,67 +17,64 @@ function createElement(html) {
 }
 
 class BucketList {
-    constructor () {
+  constructor() {}
 
-    }
-
-    _getTemplate() {
-        return `<a class="logo" href="/">Projects</a>
+  _getTemplate() {
+    return `<a class="logo" href="/">Projects</a>
           <div class="header__btns">
             <button class="btn"><i class="fa-solid fa-magnifying-glass"></i></button>
             <button class="btn"><i class="fa-solid fa-bars"></i></button>
           </div>
         </header>`;
+  }
 
-    }
-
+  get element() {
+    return this._element;
+  }
 }
 
 class Filter {
-    constructor() {
-        
+  constructor() {}
 
-    }
+  _getTemplate() {
+    return `<div class="filter"></div>`;
+  }
 
-    _getTemplate(){
-        return `  <div class="filter"></div>`;
-
-    }
+  get element() {
+    return this._element;
+  }
 }
 
-class FilterItem{
-    constructor(item) {
-        this_item = item 
-
-    }
-    _getTemplate(){
-        return `<span class="filter-item filter-item--active">All</span>
-            <span class="filter-item">Home</span>
-            <span class="filter-item">Sport</span>`;
-    }
+class FilterItem {
+  constructor(item) {
+    this_item = item;
+  }
+  _getTemplate() {
+    return `<span class="filter-item filter-item--active">All</span>`
+  }
+  get element() {
+    return this._element;
+  }
 }
 
 class Task {
-    constructor(title ,desc){
-        this_title = title
-        this_desc = desc
+    constructor({title, desc}){
+        this._title = title
+        this._desc = desc
+    }
+
+    _getTemplate(){
+        return `<article class="task">
+              <h3 class="task__title">Home</h3>
+              <p class="task__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, pariatur</p>
+            </article>`;
 
     }
-    _getTemplate(){
-        return ` <div class="bucket-list__content">
-            <article class="task">
-              <h3 class="task__title">Home</h3>
-              <p class="task__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, pariatur</p>
-            </article>
-            <article class="task">
-              <h3 class="task__title">Home</h3>
-              <p class="task__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, pariatur</p>
-            </article>
-            <article class="task">
-              <h3 class="task__title">Home</h3>
-              <p class="task__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, pariatur</p>
-            </article>
-          </div>`;
-
+    get element() {
+        return this._element
     }
 }
+
+
+const root = document.querySelector('.root')
+root.insertAdjacentElement("beforeend")
