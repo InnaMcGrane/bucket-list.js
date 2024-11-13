f5fd32563c2570f5ad2cde16a66b02e788d7fea7
InnaMcGrane <innamcgrane@gmail.com>
2024-11-13 07:38:41 +0000

    finished Filter component

--- script.js
+++ script.js
@@ -10,19 +10,18 @@
 */
 
 function createElement(html) {
-    const element = document.createElement("div");
-    element.insertAdjacentHTML("beforeend",html);
-    return element.firstElementChild;
-
+  const element = document.createElement("div");
+  element.insertAdjacentHTML("beforeend", html);
+  return element.firstElementChild;
 }
 
 class BucketList {
   constructor(tasks, Filter, FilterItem, Task) {
+    this._tasks = tasks;
     this._Filter = Filter;
     this._FilterItem = FilterItem;
     this._Task = Task;
-    this._tasks = tasks;
-    this._init()
+    this._init();
   }
 
   _getTemplate() {
@@ -35,13 +34,19 @@ class BucketList {
           </div>
         </header>
        <main class="bucket-list__main">
+       <div class="bucket-list__filter"></div>
         <div class="bucket-list__content"></div>
        </main>
     </div>`;
   }
 
-  _init(){
-    this._element = createElement(this._getTemplate())
+  _init() {
+    this._element = createElement(this._getTemplate());
+    this._render();
+  }
+
+  _render() {
+    this._element.querySelector(".bucket-list__filter").append(new this._Filter(this._FilterItem).element);
   }
 
   get element() {
@@ -52,10 +57,24 @@ class BucketList {
 class Filter {
   constructor(FilterItem) {
     this._FilterItem = FilterItem;
+    this._init();
   }
 
   _getTemplate() {
     return `<div class="filter"></div>`;
+  }
+
+  _init() {
+    this._element = createElement(this._getTemplate());
+    this._render()
+  }
+
+  _generateItems() {
+    return [new this._FilterItem({ text: "All", active: true }).element, new this._FilterItem({ text: "Home", active: false }).element, new this._FilterItem({ text: "Sport", active: false }).element];
+  }
+
+  _render() {
+    this._element.append(...this._generateItems());
   }
 
   get element() {
@@ -64,13 +83,18 @@ class Filter {
 }
 
 class FilterItem {
-  constructor({text, active}) {
+  constructor({ text, active }) {
     this._text = text;
     this._active = active;
+    this._init()
   }
 
   _getTemplate() {
-    return `<span class="filter-item ${this._active === true ? "filter-item--active" : ""}">${this._text}</span>`
+    return `<span class="filter-item ${this._active === true ? "filter-item--active" : ""}">${this._text}</span>`;
+  }
+
+  _init(){
+    this._element = createElement(this._getTemplate());
   }
 
   get element() {
@@ -79,23 +103,35 @@ class FilterItem {
 }
 
 class Task {
-    constructor({title, desc}){
-        this._title = title
-        this._desc = desc
-    }
+  constructor({ title, desc }) {
+    this._title = title;
+    this._desc = desc;
+  }
 
-    _getTemplate(){
-        return `<article class="task">
+  _getTemplate() {
+    return `<article class="task">
               <h3 class="task__title">${this._title}</h3>
               <p class="task__desc">${this._desc}</p>
             </article>`;
-    }
-    
-    get element() {
-        return this._element
-    }
+  }
+
+  get element() {
+    return this._element;
+  }
 }
 
+const tasks = [
+  {
+    id: 241,
+    title: "home title",
+    desc: "home desc",
+  },
+  {
+    id: 234,
+    title: "sport title",
+    desc: "sport desc",
+  },
+];
 
-const root = document.querySelector('.root')
-root.insertAdjacentElement("beforeend", new BucketList(Filter, FilterItem, Task).element)
+const root = document.querySelector(".root");
+root.insertAdjacentElement("beforeend", new BucketList(tasks, Filter, FilterItem, Task).element);
